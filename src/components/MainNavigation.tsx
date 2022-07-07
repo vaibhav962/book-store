e4328516598c9061ccf7a9e8f256c11165734a17
Vaibhav Chauhan <chauhanvaibhav9602@gmail.com>
2022-07-07 17:49:04 +0530

    Added : Global Search

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,8 +1,10 @@
 import React, { lazy } from "react";
 import { Route, Switch } from "react-router-dom";
+import Book from "../pages/book";
 import BookList from "../pages/book-listing";
+import EditBook from "../pages/book/editBook";
 import { RoutePaths } from "../utils/enum";
-// import PrivateRoute from "./PrivateRoute";
+import PrivateRoute from "./PrivateRoute";
 
 //component lazy loading
 const Login = lazy(() => import("../pages/login/index"));
@@ -14,6 +16,9 @@ const AppRoutes: React.FC = () => {
 			<Route exact path={RoutePaths.Login} component={Login} />
 			<Route exact path={RoutePaths.Register} component={Register} />
 			<Route exact path={RoutePaths.BookListing} component={BookList} />
+			<PrivateRoute exact path={RoutePaths.Book} component={Book} />
+			<PrivateRoute exact path={RoutePaths.AddBook} component={EditBook} />
+			<PrivateRoute exact path={RoutePaths.EditBook} component={EditBook} />
 		</Switch>
 	);
 };
